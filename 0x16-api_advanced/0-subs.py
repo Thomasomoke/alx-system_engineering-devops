3bfa541ed94917810ff984f79987b34e2eacb09a
Thomas Omoke <omokethomas644@gmail.com>
2024-08-13 05:21:02 -0700

    function that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module to query the Reddit API and returns the number of subscribers"""
+import requests
+
+def number_of_subscribers(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        data = response.json()
+        number_of_subscribers = data.get('data').get('subscribers')
+        return number_of_subscribers
+    else:
+        return 0
+
